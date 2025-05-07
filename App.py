c1bc653331203dbf0831bc297c6159921a93b67a
Scharrabbs <sarabia.schlwyn@cec.edu.ph>
2025-05-07 21:58:25 +0800

    Second Update

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
